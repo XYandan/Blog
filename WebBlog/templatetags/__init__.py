4548cb29c63281152ea23f16407b268223a561a9
XYandan <879975635@qq.com>
2020-06-29 12:37:25 +0800

    创建自定义的模板

---
+++ WebBlog/templatetags/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/29 12:29
+# @Author : ZhangYongjie
---
+++ WebBlog/templatetags/form_tags.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/29 12:29
+# @Author : ZhangYongjie
+
+"""
+
+创建自定义的模板
+
+"""
+from django import template
+
+register = template.Library()
+
+@register.filter
+def field_type(bound_field):
+    return bound_field.field.widget.__class__.__name__
+
+@register.filter
+def input_class(bound_field):
+    css_class = ''
+    if bound_field.form.is_bound:
+        if bound_field.errors:
+            css_class = 'is-invalid'
+        elif field_type(bound_field) != 'PasswordInput':
+            css_class = 'is-valid'
+    return 'form-control {}'.format(css_class)
